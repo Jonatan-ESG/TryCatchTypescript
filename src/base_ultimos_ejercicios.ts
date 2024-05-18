ba5d416c8a5d778d2ab7ac0e6562938aa686ba17
Jonatan-ESG <jona.sandovalguerra@gmail.com>
2024-05-18 09:32:55 -0600

    :construction_worker: Se agregó base para ejercicios 5 y 6

---
+++ src/base_ultimos_ejercicios.ts
@@ -0,0 +1,26 @@
+/* EJERICIO 6 */
+function leerArchivo() {}
+
+// Funciones simuladas de abrir y cerrar archivos
+function abrirArchivo(nombre: string) {
+    console.log(`Abriendo archivo: ${nombre}`)
+    return { nombre: nombre } // Simulación de objeto de archivo
+}
+
+function cerrarArchivo(archivo: any) {
+    console.log(`Cerrando archivo: ${archivo.nombre}`)
+    // Realizar acciones de cierre del archivo (simulado)
+}
+
+// Ejemplo de uso
+leerArchivo()
+
+/* EJERICIO 6 */
+function operacionCompleja(x: number, y: number): number {
+    return 0
+}
+
+function ejecutarOperacion(x: number, y: number) {}
+
+// Ejemplo de uso
+ejecutarOperacion(2, 3)
